2842b6ae3fe0885851ae0ae295c667de0916a775
fukui_takanori <itfukui0922@icloud.com>
2019-10-01 15:01:31 +0900

    fix: deployするブランチ先を毎回変更するように

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash -eu
 
+TODAY=$(date"%Y%m%d-%H:%M:%s")
 yarn build
 git checkout gh-pages
-git checkout -b gh-pages-tmp
+git checkout -b gh-pages-${TODAY}
 ls -a | grep -v -E '^build|^CNAME|^pp.txt|^.gitignore|^.git|^\.\.|^\.$' | xargs rm -rf
 mv build/* ./
 rm -d build
 git add .
 git commit -m 'yarn:deploy'
-git push --set-upstream origin gh-pages-tmp
+git push --set-upstream origin gh-pages-${TODAY}
