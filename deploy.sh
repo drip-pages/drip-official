e848ce207fc62a40ac7be67420efb3657f670e86
fukui_takanori <itfukui0922@icloud.com>
2019-10-01 14:25:47 +0900

    add: deployのbashファイル作成

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+yarn build
+git checkout gh-pages
+git checkout -b gh-pages-tmp
+ls -a | grep -v -E '^build|^CNAME|^pp.txt|^.gitignore|^.git|^\.\.|^\.$' | xargs rm -rf
+mv build/* ./
+rm -d build
+git add .
+git commit -m 'yarn:deploy'
+git push --set-upstream origin gh-pages-tmp
