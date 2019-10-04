5b317cbe0e89b7ae32c33276f257c04a4813460b
fukui_takanori <itfukui0922@icloud.com>
2019-10-04 13:59:04 +0900

    change: newsItem.jsonの読み込み場所を各コンポーネントに分けるようにした

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -3,16 +3,12 @@ import { Link } from 'react-router-dom'
 import './News.scss'
 import NewsList from '../NewsList'
 import Slide from '../Slide'
-
-type NewsProps = {
-  items: any[]
-}
-
-const News = (props: NewsProps) => (
+import NewsItems from '../../data/newsItems.json'
+const News = () => (
   <Slide className="News">
     <h2>News</h2>
     <div className="content">
-      <NewsList items={props.items.slice(0, 3)} />
+      <NewsList items={NewsItems.slice(0, 3)} />
     </div>
     <Link className="read-more" to="/news">
       Read More...
