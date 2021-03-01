25488313dd38e22326117fc22bda263fa85923de
ogawa_mitsunori <mitsunori.ogawa@drecom.co.jp>
2021-03-01 15:31:54 +0900

    M: LoveChain およびメンバー1名削除

--- src/components/OurInvention/index.tsx
+++ src/components/OurInvention/index.tsx
@@ -4,8 +4,6 @@ import arowBigLogo from '../../img/arow.png'
 import arowSmallLogo from '../../img/ArowLogo.png'
 import pillarsBigLogo from '../../img/6-pillars.png'
 import pillarsSmallLogo from '../../img/pillarsSmall.png'
-import loveChainBigLogo from '../../img/lovechain.png'
-import loveCahinSmallLogo from '../../img/loveChainSmallLogo.png'
 import './OurInvention.scss'
 import { useTranslation } from 'react-i18next'
 import Slide from '../Slide'
@@ -27,13 +25,6 @@ const OurInvention = () => {
         <p>{t('inventionPillars_1')}</p>
         <a href="https://6-pillars.ooo/" target="_blank" rel="noopener noreferrer">
           https://6-pillars.ooo/
-        </a>
-      </Invention>
-      <Invention className="loveChain" bigLogo={loveChainBigLogo} smallLogo={loveCahinSmallLogo}>
-        <strong>{t('inventionLove_1')}</strong>
-        <p>{t('inventionLove_2')}</p>
-        <a href="https://lovechain.ooo/" target="_blank" rel="noopener noreferrer">
-          https://lovechain.ooo/
         </a>
       </Invention>
     </div>
--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -11,8 +11,6 @@ import necoAni from '../../img/neco-ani.gif'
 import ogw from '../../img/police.png'
 import ogwAni from '../../img/police-ani.gif'
 import './Team.scss'
-import pieer from '../../img/pieer.jpg'
-import pieerAni from '../../img/pieer-ani.png'
 import tk from '../../img/tk.jpeg'
 import tkAni from '../../img/tk-ani.png'
 import owner from '../../img/owner.jpg'
@@ -43,7 +41,6 @@ const Team = () => {
         <Member className="member-icon" imgFile={ogw} movieFile={ogwAni} name="Engineer" />
         <Member className="member-icon" imgFile={master} movieFile={masterAni} name="Master" />
         <Member className="member-icon" imgFile={owner} movieFile={ownerAni} name="Engineer" />
-        <Member className="member-icon" imgFile={pieer} movieFile={pieerAni} name="Engineer & Planner" />
         <Member className="member-icon" imgFile={tk} movieFile={tkAni} name="Engineer" />
         <Member className="member-icon" imgFile={blackmask} movieFile={blackmaskAni} name="Designer" />
         <Member className="member-icon" imgFile={flyup} movieFile={flyupAni} name="Planner" />
